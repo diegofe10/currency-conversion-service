53472b24c91870158e966bd5fbb7a0cbe67ac696
Diego Gean da Fé <diegogeandafe@gmail.com>
2020-05-20 20:56:10 -0300

    Atualizado pom.xml

--- src/main/java/br/com/currency/conversion/service/CurrencyConversionServiceApplication.java
+++ src/main/java/br/com/currency/conversion/service/CurrencyConversionServiceApplication.java
@@ -3,6 +3,7 @@ package br.com.currency.conversion.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
@@ -11,6 +12,7 @@ import brave.sampler.Sampler;
 @SpringBootApplication
 @EnableFeignClients("com.in28minutes.microservices.currencyconversionservice")
 @EnableDiscoveryClient
+@EnableHystrix
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
